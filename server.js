169aeb7686a0af7e14775f9b6be060813cf5e3f7
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2022-11-13 00:24:39 -0300

    :sparkles: Add new payload types to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { WebSocketServer } = require("ws");
 
+// Parse .env file
 require("dotenv").config();
 
 const serverInfo = {
@@ -7,6 +8,7 @@ const serverInfo = {
   port: process.env.SERVER_PORT ?? 3000,
 };
 
+// Create WebSocket server
 const server = new WebSocketServer(
   {
     port: serverInfo.port,
@@ -16,9 +18,19 @@ const server = new WebSocketServer(
   }
 );
 
+// Global objects to store game data
 let players = {};
 
+let hp = {};
+
+let score = {};
+
+// Global functions
+
+function appendDefaultValues() {}
+
 function sendAll(payload) {
+  // Send the payload to all connected clients
   server.clients.forEach(client => {
     client.send(JSON.stringify(payload));
   });
@@ -26,8 +38,10 @@ function sendAll(payload) {
 }
 
 /*
+Sever payload
+
 data = {
-  type: "player" | "bullet" | "watcher",
+  type: "player" | "hit" | "bullet" | "watcher",
   payload: {
     // Player payload
     id: string,
@@ -36,44 +50,84 @@ data = {
     rot: number[],
     color: number[]
 
-    // Bullet payload
+    // Hit payload
     origin: string (player id),
-    target: string (player id),
-    hit: boolean
+    target: string (player id)
+
+    // Bullet payload
+    pos: number[],
+    rot: number[]
   }
 }
 */
 
 server.on("connection", client => {
+  // Define client variables
   let cache = undefined;
   let playerId = undefined;
 
   client.on("message", data => {
     data = data.toString();
 
+    // Simple cache
     if (data !== cache) {
       cache = data;
+
+      // Parse received data
       data = JSON.parse(data);
       const payload = data.payload ?? {};
 
       switch (data.type) {
         // Player connection
         case "player":
+          // Initialize client variables
           if (playerId === undefined) {
             playerId = payload.id;
+
+            hp[playerId] = 100;
+            score[playerId] = 0;
           }
 
+          // Populate global players object
           players[playerId] = payload;
-          sendAll(players);
 
+          players[playerId].hp = hp[playerId];
+          players[playerId].score = score[playerId];
+
+          // Send data to all clients on each player message
+          sendAll(players);
           break;
 
-        // Bullet connection
-        case "bullet":
+        // Hit connection
+        case "hit":
+          // Hit variables
+          const { origin, target } = payload;
+          const damage = 20;
+
+          // Apply damage to target player
+          if (Object.keys(hp).includes(target)) {
+            hp[target] -= damage;
+
+            // Check target player death
+            if (hp[target] <= 0) {
+              hp[target] = 0;
+
+              // Update score
+              if (Object.keys(score).includes(origin)) {
+                score[origin]++;
+                players[origin].score = score[origin];
+              }
+            }
+
+            players[target].hp = hp[target];
+          }
+
+          sendAll(players);
           break;
 
         // Watcher connection
         case "watcher":
+          // Send initial payload only to the watcher client
           client.send(JSON.stringify(players));
           break;
       }
@@ -82,7 +136,11 @@ server.on("connection", client => {
 
   client.on("close", () => {
     if (playerId !== undefined) {
+      // Remove player from server and notify clients
       delete players[playerId];
+      delete hp[playerId];
+      delete score[playerId];
+
       sendAll(players);
     }
   });
